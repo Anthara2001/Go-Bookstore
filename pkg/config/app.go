afc7bc63ad2142072e168a0688558d52687aeeef
Anthara Gangone <anthara_gangone@epam.com>
2024-05-30 15:44:51 +0530

    Made changes near opening the mysql connection

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -8,7 +8,7 @@ import (
 var db *gorm.DB
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:root/BooksData?")
+	d, err := gorm.Open("mysql", "root:root/BooksData?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
