97bdf1f2fba42f448e2b65b221284d65acf71fde
Anthara Gangone <anthara_gangone@epam.com>
2024-05-30 16:28:32 +0530

    Done with reading and unmarshalling the json data

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,1 +1,20 @@
 package utils
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func ParseBody(req *http.Request, x interface{}) error {
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(body, x)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
